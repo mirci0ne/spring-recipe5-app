a3626e14343a523f9ba8d588a23aea5635bcdaf3
Mircea Manole <mircea1406@gmail.com>
2022-01-06 23:02:07 +0200

    - spring configuration

---
+++ src/main/java/guru/springframework/springrecipe5app/SpringRecipe5AppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrecipe5app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipe5AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecipe5AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/springrecipe5app/SpringRecipe5AppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrecipe5app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipe5AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
