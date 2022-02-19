e3144a46cd336a86ab5ba9d0811c43437b61b79a
Mircea Manole <mircea1406@gmail.com>
2022-02-19 17:50:51 +0200

    - included webjars dependencies

--- src/main/java/guru/springframework/springrecipe5app/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/springrecipe5app/services/RecipeServiceImpl.java
@@ -5,6 +5,7 @@ import guru.springframework.springrecipe5app.repositories.RecipeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
@@ -21,5 +22,16 @@ public class RecipeServiceImpl implements RecipeService {
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
+    }
+
+    @Override
+    public Recipe findById(Long l) {
+
+        Optional<Recipe> recipeOptional = recipeRepository.findById(l);
+        if (!recipeOptional.isPresent()) {
+            throw new RuntimeException("Recipe not found");
+        }
+
+        return recipeOptional.get();
     }
 }
