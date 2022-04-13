3b6b0728745bc57b4471d8f67ca97503350ca4b1
Mircea Manole <mircea1406@gmail.com>
2022-04-13 22:11:49 +0300

    - implemented not found exception

--- src/main/java/guru/springframework/springrecipe5app/controllers/RecipeController.java
+++ src/main/java/guru/springframework/springrecipe5app/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package guru.springframework.springrecipe5app.controllers;
 
 import guru.springframework.springrecipe5app.commands.RecipeCommand;
+import guru.springframework.springrecipe5app.exceptions.NotFoundException;
 import guru.springframework.springrecipe5app.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -51,5 +54,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception) {
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 }
